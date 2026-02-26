729020103977fd8e06cd07cce27c3a5fb7aa1155
SalanaDev <salanagomes.ti@gmail.com>
2026-02-26 13:02:38 -0300

    feat: cria função criar_produto com validações

--- backend/produtos.py
+++ backend/produtos.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+def criar_produto(nome, preco, quantidade):
+
+# Cria um novo produto e salva no dados.txt
+# Recebe nome, preco e quantidade do produto.
+
+    if nome == "":
+        print("Nome não pode estar vazio.")
+        return
+    if preco <= 0:
+        print("Preço deve ser maior que zero.")
+        return
+    if quantidade < 0:
+        print("A quantidade não pode ser negativa.")
+        return
+    
+    # Pega o ID baseado em quantos produtos existem.
+    try:
+        with open("dados.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+            id_novo = len(linhas) + 1
+    except FileNotFoundError:
+        id_novo = 1
+
+    # Salva o produto no txt
+    with open("dados.txt", "a") as arquivo:
+        arquivo.write(f"{id_novo},{nome},{preco},{quantidade}\n")
+
+    print(f"Produto '{nome}' criado!")
