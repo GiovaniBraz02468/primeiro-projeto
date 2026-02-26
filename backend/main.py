5b962bd56b8c406598857c2407049bd11b6d5cc6
Allan Lopes <jrjackson563@gmail.com>
2026-02-25 22:57:18 -0300

    feat: criação main e mostrar menu
    Criei o menu que irá ser mostrado para o usuario e o main que ira criar a interação com o usuario

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,68 @@
-print("teste")
-print("teste2")
+from produtos import *
+
+# MENU E CONTROLE DO SISTEMA
+
+def mostrar_menu():
+    print('\n ===== SISTEMA DE PRODUTOS =====')
+    print('1 - Criar produto')
+    print('2 - Listar produto')
+    print('3 - Atualizar produto')
+    print('4 - Deletar produto')
+    print('5 - Sair')
+
+def main():
+
+    while True:
+        mostrar_menu()
+
+        try:
+            opcao = int(input('Escolha uma opção: '))
+        except ValueError:
+            print('Digite apenas números!')
+            continue
+
+        if opcao == 1:
+            nome =  input('Nome do produto: ')
+            try:
+                preco = float(input('Preço: '))
+                quantidade = int(input('Quantidade: '))
+            except ValueError:
+                print('Preço e quantidade devem ser números!')
+                continue
+
+        # função criar(nome, preco, quantidade)
+        elif opcao == 2:
+            #função listar produtos()
+            pass
+
+        elif opcao == 3:
+            try:
+                id = int(input('ID do produto: '))
+                nome = str(input('Novo nome: '))
+                preco = float(input('Novo preço: '))
+                quantidade = int(input('Nova quantidade: '))
+            except ValueError:
+                print('Valor inválido!')
+                continue
+
+        #função atualizar o produto(id, nome, preco, quantidade)
+
+        elif opcao == 4:
+            try:
+                id = int(input('Digite o ID do produto a ser removido: '))
+            except ValueError:
+                print('ID invalido')
+                continue
+
+        #função deletar produto(id)
+
+        elif opcao == 5:
+            print('Volte sempre!')
+            print('Saindo do sistema...')
+            break
+
+        else:
+            print('Ops... Opção inválida. Tente novamente')
+
+
+main()
